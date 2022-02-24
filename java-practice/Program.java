0910dd6f8fe39f1391cf1614d342d3c40362fafb
vitaliysolomonov <454115@gmail.com>
2022-02-24 11:31:24 -0600

    initial commit

---
+++ java-practice/Program.java
@@ -0,0 +1,11 @@
+package Programcom.galvanize;
+
+//import java.sql.SQLOutput;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
